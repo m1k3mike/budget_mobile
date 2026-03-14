d5910dd95825c0314727e5227034dbf7f5da68da
m1k3mike <m1k3mike@example.com>
2026-03-14 22:50:38 +0300

    Deploy to GitHub Pages via docs folder

---
+++ docs/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Basic service worker that does not interfere with network.
+});
